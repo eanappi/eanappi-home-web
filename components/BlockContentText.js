ec0da94717ea565e8fb53ba6c4c31ffe968b4b30
Ernesto A. Nappi <eanappi@gmail.com>
2019-04-25 15:30:09 -0300

    FIX - Content text upgrade in about section

--- components/BlockContentText.js
+++ components/BlockContentText.js
@@ -1,20 +1,21 @@
 const BlockContentText = {
   view(vnode) {
     return m(`div${vnode.attrs.id}`,
-      m('.row.container', 
-        m('.col.s12', 
-          m('h6', 
-            m('i.material-icons', 'mode_comment'),
+      m(".row.container", 
+        m(".col.s12", 
+          m("h6", 
+            m("i.material-icons", "mode_comment"),
             `${vnode.attrs.title}`
           )
         ),
-        m('.col.s12', 
-          m('h4', `${vnode.attrs.description}`)
+        m(".col.s12", 
+          m("h4", `${vnode.attrs.description}`)
         ),
-        m('.col.s12',
-          m('address',
+        m(".col.s12",
+          m("address",
             m(`a[href="mailto:${vnode.attrs.email}"]`, 
-              `${vnode.attrs.email}`)
+              `${vnode.attrs.email}`
+            )
           )
         ),
       )
--- components/Home.js
+++ components/Home.js
@@ -2,16 +2,16 @@ const Home = {
   view() {
     return m(Layout, [
       m(BlockParallaxText, {
-        id: '#parallax-container-header',
-        image: 'img/bg-abstract.jpg',
-        title: 'Alejandro Nappi',
-        description: 'Web & Creative Developer for the world'   
+        id: "#parallax-container-header",
+        image: "img/bg-abstract.jpg",
+        title: "Alejandro Nappi",
+        description: "Web & Creative Developer for the world"   
       }),
       m(BlockContentText, {
-        id: '#block-content-text-about',
-        title: ' HELLO EVERYONE',
-        description: 'Hi! I am Felicity, a multi-disciplinary designer based out of the beautiful New York City and I design beautiful and functional websites and mobile applications.',
-        email: 'eanappi@gmail.com'
+        id: "#block-content-text-about",
+        title: " HELLO EVERYONE",
+        description: "Hi, I'm Alejandro, a multidisciplinary web developer based in the beautiful city of Buenos Aires and I develop beautiful and functional websites. My developments use a modern web development architecture based on JavaScript on the client side, reusable APIs and precompiled markup.",
+        email: "eanappi@gmail.com"
       })
     ])
   }
