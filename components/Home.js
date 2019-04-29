03eabf6a9ed8195f598a40a26908bcd6f1532548
Ernesto A. Nappi <eanappi@gmail.com>
2019-04-29 11:32:42 -0300

    Use DB file for manage all content

--- components/Home.js
+++ components/Home.js
@@ -5,30 +5,12 @@ const Home = {
   },
   view() {
     return m(Layout, [
-      m(BlockParallaxText, {
-        id: "#parallax-container-header",
-        image: "img/bg-abstract-web.jpg",
-        title: "Alejandro Nappi",
-        description: "Desarrollador web y creativo para el mundo."   
-      }),
-      m(BlockContentText, {
-        id: "#block-content-text-about",
-        title: " HOLA A TODOS",
-        description: "Hola, soy Alejandro, un desarrollador web multidisciplinario con sede en la bella ciudad de Buenos Aires y desarrollo sitios web hermosos y funcionales. Mis desarrollos utilizan una arquitectura moderna para desarrollo web basada en JavaScript en el lado del cliente, API reutilizables y markup precompilado.",
-        email: "eanappi@gmail.com"
-      }),
+      m(BlockParallaxText, Db.row1),
+      m(BlockContentText, Db.row2),
       m(BlockGalleryImage),
-      m(BlockContentText, {
-        id: "#block-content-text-experience",
-        title: " EXPERIENCIA LABORAL",
-        description: "Tengo más de 15 años de experiencia y tuve el placer de trabajar con algunas de las mejores empresas de todo el mundo.",
-      }),
-      m(BlockGalleryImageCards),
-      m(BlockContentText, {
-        id: "#block-content-text-contact",
-        title: " ESTAR EN CONTACTO",
-        description: "Me encanta escuchar y estoy ansioso por hablar con usted sobre su proyecto. Póngase en contacto si tiene alguna consulta y le responderé lo antes posible.",
-      }),
+      m(BlockContentText, Db.row4),
+      m(BlockCollectionAvatarContent),
+      m(BlockContentText, Db.row6),
     ])
   }
 }
---
+++ db.js
@@ -0,0 +1,30 @@
+const Db = {
+  row1: {
+    id: "#parallax-container-header",
+    image: "img/bg-abstract-web.jpg",
+    title: "Alejandro Nappi",
+    description: "Desarrollador web y creativo para el mundo."   
+  },
+  row2: {
+    id: "#block-content-text-about",
+    title: " HOLA A TODOS",
+    description: "Hola, soy Alejandro, un desarrollador web multidisciplinario con sede en la bella ciudad de Buenos Aires y desarrollo sitios web hermosos y funcionales. Mis desarrollos utilizan una arquitectura moderna para desarrollo web basada en JavaScript en el lado del cliente, API reutilizables y markup precompilado.",
+    email: "eanappi@gmail.com"
+  },
+  row3: {
+
+  },
+  row4: {
+    id: "#block-content-text-experience",
+    title: " EXPERIENCIA LABORAL",
+    description: "Tengo más de 15 años de experiencia y tuve el placer de trabajar con algunas de las mejores empresas de todo el mundo.",
+  },
+  row5: {
+
+  },
+  row6: {
+    id: "#block-content-text-contact",
+    title: " ESTAR EN CONTACTO",
+    description: "Me encanta escuchar y estoy ansioso por hablar con usted sobre su proyecto. Póngase en contacto si tiene alguna consulta y le responderé lo antes posible.",
+  }
+}
